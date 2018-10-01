1b7e8185a7575aa2eefdb03e6009398ace100ee1
Matti Lankinen <m.lankinen@iki.fi>
2018-10-02 00:51:42 +0300

    Wait until test server has been started and running

--- test/todomvc_server.clj
+++ test/todomvc_server.clj
@@ -3,8 +3,16 @@
             [clojure.java.io :as io]
             [compojure.core :refer [routes GET]]
             [compojure.route :refer [files not-found]]
-            [org.httpkit.server :refer [run-server]])
+            [org.httpkit.server :refer [run-server]]
+            [org.httpkit.client :as http])
   (:import (java.io Closeable)))
+
+(defn- server-running? [port]
+  (try
+    (= 200 (:status @(http/get (str "http://127.0.0.1:" port))))
+    (catch Exception e
+      (.printStackTrace e)
+      false)))
 
 (defn start-server! [port]
   (let [app-dir (io/file "test/todomvc")
@@ -15,5 +23,11 @@
                   (files "/static" {:root (str (.getAbsolutePath app-dir) "/resources/public")})
                   (not-found "..."))
         stop!   (run-server handler {:ip "127.0.0.1" :port port})]
+    (if-not (loop [n 30]
+              (or (server-running? port)
+                  (and (pos? n)
+                       (do (Thread/sleep 100)
+                           (recur (dec n))))))
+      (throw (RuntimeException. "Could not start server")))
     (reify Closeable
       (close [_] (stop!)))))

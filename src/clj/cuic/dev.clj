c89a1a7c4214fc95fd7c2ea13162eed95639bdab
Matti Lankinen <m.lankinen@iki.fi>
2018-10-16 01:54:49 +0300

    Update function contracts from development utilities

--- src/clj/cuic/dev.clj
+++ src/clj/cuic/dev.clj
@@ -15,7 +15,7 @@
   (alter-var-root #'c/*browser* (constantly browser))
   nil)
 
-(defn update-config!
+(defn swap-config!
   "Forcefully updates the default configurations with the given function."
   [f & args]
   (println "Forcefully changing the default configuration. I hope you know what you're doing... ;-)")
@@ -24,17 +24,17 @@
   (pprint c/*config*)
   nil)
 
-(defn launch!!
+(def default-launch-opts
+  {:headless             false
+   :additional-arguments {"window-position"             "0,35"
+                          "auto-open-devtools-for-tabs" "true"}})
+
+(defn launch-as-default!
   "Launches a new (visible) browser and sets it to the default browser."
-  ([width height headless? opts]
-   (doto (c/launch! (merge {:window-size {:width width :height height} :headless headless?} opts))
+  ([opts]
+   (doto (c/launch! (merge default-launch-opts opts))
      (set-browser!)))
-  ([width height headless?]
-   (launch!! width height headless? {}))
-  ([width height]
-   (launch!! width height false))
-  ([] (doto (c/launch! {:headless false})
-        (set-browser!))))
+  ([] (launch-as-default! {})))
 
 (defn preview-image [image]
   {:pre [(instance? BufferedImage image)]}

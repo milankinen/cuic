89d3d59f077217a3db5a39626b9b925faf0d63bd
Matti Lankinen <m.lankinen@iki.fi>
2018-11-20 23:55:21 +0200

    Add test filtering support to CLI

--- test/test_runner.clj
+++ test/test_runner.clj
@@ -1,8 +1,15 @@
 (ns test-runner
-  (:require [eftest.runner :refer [find-tests run-tests]]))
+  (:require [clojure.string :as string]
+            [eftest.runner :refer [find-tests run-tests]]))
 
-(defn run-tests-cli [& _]
-  (println "Running tests...")
-  (let [res (-> (find-tests "test")
-                (run-tests {:multithread? false}))]
+(defn- include [names]
+  (fn [t]
+    (let [test-name (str (:ns (meta t)) "/" (:name (meta t)))]
+      (some #(string/includes? test-name %) names))))
+
+(defn run-tests-cli [& names]
+  (let [tests (->> (find-tests "test")
+                   (filter (if (seq names) (include names) (constantly true))))
+        _     (println "Running" (count tests) "tests...")
+        res   (run-tests tests {:multithread? false})]
     (System/exit (+ (:fail res) (:error res)))))

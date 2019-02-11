f4d7219eb54b4c10b807c3c453c37a1f7d8360bb
Matti Lankinen <m.lankinen@iki.fi>
2019-02-11 20:03:19 +0200

    Disable cache by default for development browser

--- src/clj/cuic/dev.clj
+++ src/clj/cuic/dev.clj
@@ -2,7 +2,8 @@
   (:require [clojure.pprint :refer [pprint]]
             [cuic.core :as c]
             [cuic.impl.dom-node :refer [node?]]
-            [cuic.impl.js-bridge :as js]))
+            [cuic.impl.js-bridge :as js]
+            [cuic.impl.browser :as browser]))
 
 (defn set-browser!
   "Forcefully sets the default browser to the given one so that every core
@@ -26,10 +27,18 @@
    :additional-arguments {"window-position"             "0,35"
                           "auto-open-devtools-for-tabs" "true"}})
 
+(defn disable-cache!
+  "Disables cache from the given browser instance"
+  [browser]
+  (-> (browser/tools browser)
+      (.getNetwork)
+      (.setCacheDisabled true)))
+
 (defn launch-as-default!
   "Launches a new (visible) browser and sets it to the default browser."
   ([opts]
    (doto (c/launch! (merge default-launch-opts opts))
+     (disable-cache!)
      (set-browser!)))
   ([] (launch-as-default! {})))
 

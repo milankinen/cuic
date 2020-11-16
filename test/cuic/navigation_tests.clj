de332d0829752371c8cd958bcdf111bd13b54e40
Matti Lankinen <m.lankinen@iki.fi>
2020-11-16 20:27:08 +0200

    Add tests for browser navigation

---
+++ test/cuic/navigation_tests.clj
@@ -0,0 +1,30 @@
+(ns cuic.navigation-tests
+  (:require [clojure.test :refer :all]
+            [cuic.core :as c]
+            [cuic.test :refer [deftest* is* browser-test-fixture]]
+            [cuic.test-common :refer [forms-test-fixture]]))
+
+(use-fixtures
+  :once
+  (browser-test-fixture)
+  (forms-test-fixture))
+
+(defn- title []
+  (c/eval-js "this.title" {} (c/document)))
+
+(deftest* go-forward-backward-tests
+  (testing "go-back returns boolean whether going back was possible or not"
+    (is* (= "Forms test" (title)))
+    (is (true? (c/go-back)))
+    (is* (= "" (title)))
+    (is (false? (c/go-back))))
+  (testing "go-forward returns boolean whether going forward was possible or not"
+    (is (true? (c/go-forward)))
+    (is* (= "Forms test" (title)))
+    (is (false? (c/go-forward)))
+    (is* (= "Forms test" (title))))
+  (testing "goto reloads whole page"
+    (c/click (c/find "#delayed-node-trigger"))
+    (is* (some? (c/query "#delayed-node")))
+    (c/goto (c/eval-js "this.location.href"))
+    (is* (nil? (c/query "#delayed-node")))))

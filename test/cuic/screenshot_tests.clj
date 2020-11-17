b6cd45e6a10800ed1e5e3bd14bf620bae2fd9794
Matti Lankinen <m.lankinen@iki.fi>
2020-11-17 20:49:46 +0200

    Add tests for screenshots

---
+++ test/cuic/screenshot_tests.clj
@@ -0,0 +1,30 @@
+(ns cuic.screenshot-tests
+  (:require [clojure.test :refer :all]
+            [clojure.string :as string]
+            [cuic.core :as c]
+            [cuic.test :refer [deftest* is* browser-test-fixture]]
+            [cuic.test-common :refer [todos-url]])
+  (:import (javax.imageio ImageIO)
+           (java.io ByteArrayInputStream)))
+
+(use-fixtures
+  :once
+  (browser-test-fixture))
+
+(defn- get-format [image-data]
+  (with-open [iis (ImageIO/createImageInputStream (ByteArrayInputStream. image-data))]
+    (some->> (iterator-seq (ImageIO/getImageReaders iis))
+             (first)
+             (.getFormatName)
+             (string/lower-case))))
+
+(deftest* screenshot-format-tests
+  (c/goto todos-url)
+  (testing "default screenshot"
+    (is (= "png" (get-format (c/screenshot)))))
+  (testing "jpeg screenshot"
+    (is (contains? #{"jpeg" "jpg"} (get-format (c/screenshot {:format :jpeg}))))
+    (is (< (count (c/screenshot {:format  :jpeg
+                                 :quality 10}))
+           (count (c/screenshot {:format  :jpeg
+                                 :quality 100}))))))
